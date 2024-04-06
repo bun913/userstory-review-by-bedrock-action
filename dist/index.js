a0a0a9012a26fd4a72890d99f3e1562c595dfaa0
bun913 <imaizumi.taiki@classmethod.jp>
2024-04-06 16:25:12 +0900

    fix: build codes

--- dist/index.js
+++ dist/index.js
@@ -45069,15 +45069,39 @@ exports.orderMessage = `
 /***/ }),
 
 /***/ 621:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getSystemPrompt = exports.getRegion = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 const constants_1 = __nccwpck_require__(9042);
 const getRegion = () => {
-    const region = process.env.BEDROCK_REGION;
+    const region = core.getInput('bedrock_region');
     if (!region) {
         return 'us-east-1';
     }
@@ -45091,7 +45115,7 @@ const getSystemPrompt = () => {
 };
 exports.getSystemPrompt = getSystemPrompt;
 const getCheckList = () => {
-    const checkList = process.env.CHECK_LIST;
+    const checkList = core.getInput('check_list');
     if (!checkList) {
         return `<checklist>${constants_1.defaultCheckList}</checklist>`;
     }
