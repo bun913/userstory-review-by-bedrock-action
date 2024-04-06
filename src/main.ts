57e6134c2e680321cb5ec4cc4c3b036423ecff8e
bun913 <imaizumi.taiki@classmethod.jp>
2024-04-06 16:51:21 +0900

    fix: output

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ export async function run(): Promise<void> {
   try {
     const client = new BedrockRuntime({ region: getRegion() })
     const reviewdResult = await reviewByAI(client, notOkUserStory)
-    console.log(reviewdResult)
+    core.setOutput('reviewedResult', reviewdResult)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
